87aa30f4445f104be02609c3072a71bba2125509
Rick Halterman <halterman@southern.edu>
2022-01-12 10:55:15 -0500

    Version: Wed Jan 12 10:55:14 EST 2022

---
+++ Chapter01/Second.java
@@ -0,0 +1,11 @@
+public class Second {
+    public static void main(String[] args) {
+        System.out.println("   00000");
+        System.out.println("  0     0");
+        System.out.println("  0     0");
+        System.out.println("   00000");
+        System.out.println("  0     0");
+        System.out.println("  0     0");
+        System.out.println("   00000");
+    }
+}
