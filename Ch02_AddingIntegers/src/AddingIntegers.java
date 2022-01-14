f8cace529a868b46e97650d41798a207982f0339
Rick Halterman <halterman@southern.edu>
2022-01-14 11:02:17 -0500

    Version: Fri Jan 14 11:02:17 EST 2022

---
+++ Ch02_AddingIntegers/src/AddingIntegers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class AddingIntegers {
+    public static void main(String[] args) throws Exception {
+        int num1, num2;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter an integer: ");
+        num1 = scan.nextInt();
+        System.out.print("Please enter another integer: ");
+        num2 = scan.nextInt();
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+    }
+}
