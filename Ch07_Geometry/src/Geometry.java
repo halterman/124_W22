4cc544c11b62fe727b96e4358452f2f903532e73
Rick Halterman <halterman@southern.edu>
2022-03-25 10:52:42 -0400

    Version: Fri Mar 25 10:52:41 EDT 2022

--- Ch07_Geometry/src/Geometry.java
+++ Ch07_Geometry/src/Geometry.java
@@ -16,5 +16,18 @@ public class Geometry {
         System.out.println("The distance between pt1 and pt2 is " 
                             + pt1.distance(pt2));
         System.out.println(pt1);
+
+        Point p3 = Point.ORIGIN;
+        System.out.println("p3 is " + p3);
+        System.out.println("The origin is at " + Point.ORIGIN);
+
+        Point p4 = new Point(2, 3);
+        Point p5 = new Point(2, 3);
+
+        if (p4.equals(p5)) {
+            System.out.println(p4 + " and " + p5 + " are the same");
+        } else {
+            System.out.println(p4 + " and " + p5 + " are different");
+        }
     }
 }
--- Ch07_Geometry/src/Point.java
+++ Ch07_Geometry/src/Point.java
@@ -3,8 +3,21 @@
  * Point objects are immutable.
  */
 public class Point {
+    /** The <em>x</em> component of the point */
     public final double x;
+    /** The <em>y</em> component of the point */
     public final double y;
+
+    /** The point (0, 0); that is, the origin of the coordinate system */
+    public static final Point ORIGIN = new Point(0, 0);
+
+    /** Specifies the tolerance when comparing floating-point numbers for equality */
+    private static final double CLOSE_ENOUGH = 0.00001;
+
+    /** Can add a javadoc comment here ... */
+    private boolean floatEquals(double num1, double num2, double tolerance) {
+        return num1 == num2 || Math.abs(num1 - num2) < tolerance;
+    }
 
     /**
      * Initializes a {@code Point} object
@@ -17,12 +30,25 @@ public class Point {
         this.y = y;
     }
 
+
+    /**
+     * Computes the distance between this point and another point
+     * @param other the point to compute the distance to
+     * @return the distance to the other point
+     */
     public double distance(Point other) {
         double xDiff = other.x - this.x,
                yDiff = other.y - this.y;
         return Math.sqrt(xDiff*xDiff + yDiff*yDiff);
     }
 
+    public boolean equals(Point p) {
+        return floatEquals(x, p.x, CLOSE_ENOUGH) && floatEquals(y, p.y, CLOSE_ENOUGH);
+    }
+
+    /**
+     * Returns a human-readable representation of a point object
+     */
     public String toString() {
         return "(" + x + "," + y + ")";
     }
