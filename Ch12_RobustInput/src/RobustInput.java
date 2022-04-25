b817039ae8c8073d0a18eaffb0cc32f2f339a088
Rick Halterman <halterman@southern.edu>
2022-04-25 10:52:27 -0400

    Version: Mon Apr 25 10:52:25 EDT 2022

--- Ch12_RobustInput/src/RobustInput.java
+++ Ch12_RobustInput/src/RobustInput.java
@@ -1,12 +1,22 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class RobustInput {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
-        int input;
+        int input = 0;
+        boolean goodInput = false;
         System.out.print("Please enter an integer: ");
-        input = scan.nextInt();
-        scan.close();
+        while (!goodInput) {
+            try {
+                input = scan.nextInt();
+                goodInput = true;
+                scan.close();
+            } catch (InputMismatchException ex) {
+                System.out.print("Illegal form entered; please try again: ");
+                scan.nextLine();
+            }
+        }
         System.out.println("You entered " + input);
     }
 }
